a6c5cccc0988b5ff63db30a7d2aa683f4d697a6e
Tony Mendoza <tmendoza@superbadmofo.com>
2020-04-01 18:15:49 -0500

    Initial entry

---
+++ kern/devtab.c
@@ -0,0 +1,39 @@
+#include "u.h"
+#include "lib.h"
+#include "dat.h"
+#include "fns.h"
+#include "error.h"
+
+extern Dev consdevtab;
+extern Dev rootdevtab;
+extern Dev pipedevtab;
+extern Dev ssldevtab;
+extern Dev tlsdevtab;
+extern Dev mousedevtab;
+extern Dev drawdevtab;
+extern Dev ipdevtab;
+extern Dev fsdevtab;
+extern Dev mntdevtab;
+extern Dev lfddevtab;
+extern Dev audiodevtab;
+extern Dev kbddevtab;
+extern Dev cmddevtab;
+
+Dev *devtab[] = {
+	&rootdevtab,
+	&consdevtab,
+	&pipedevtab,
+	&ssldevtab,
+	&tlsdevtab,
+	&mousedevtab,
+	&drawdevtab,
+	&ipdevtab,
+	&fsdevtab,
+	&mntdevtab,
+	&lfddevtab,
+	&audiodevtab,
+	&kbddevtab,
+	&cmddevtab,
+	0
+};
+
